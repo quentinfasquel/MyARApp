a3afc40d7c8f07c47f5ce211c2ad8cabbd9ea62b
Quentin Fasquel <quentin.fasquel@gmail.com>
2018-09-03 15:51:12 +0200

    Updating ARSceneViewController to work with artoolkitX

--- MyARApp/ARSceneViewController.mm
+++ MyARApp/ARSceneViewController.mm
@@ -7,9 +7,22 @@
 //
 
 #import "ARSceneViewController.h"
-#import "ARToolkitExtensions.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#include <ARX/ARController.h>
+#pragma GCC diagnostic pop
+#import <OpenGLES/ES2/gl.h>
 
-#include <AR6/ARController.h>
+SCNMatrix4 SCNMatrix4Make(const ARdouble mat[16]) {
+  return {
+    mat[0], mat[1], mat[2], mat[3],
+    mat[4], mat[5], mat[6], mat[7],
+    mat[8], mat[9], mat[10], mat[11],
+    mat[12], mat[13], mat[14], mat[15],
+  };
+}
+
+#pragma mark -
 
 @interface ARSceneViewController () <SCNSceneRendererDelegate> {
   ARController *arController;
@@ -22,6 +35,7 @@
   int32_t viewport[4];
   std::vector<int> trackableIds;
   std::vector<bool> trackableVisible;
+  SCNMatrix4 projectionMatrix;
 }
 @property (weak, nonatomic) SCNNode *cameraNode;
 @property (strong, nonatomic) NSMutableArray<SCNNode *> *trackableNodes;
@@ -56,7 +70,7 @@
   CGFloat scale = [UIScreen mainScreen].nativeScale;
   contextWidth = (int)CGRectGetWidth([UIScreen mainScreen].bounds) * scale;
   contextHeight = (int)CGRectGetHeight([UIScreen mainScreen].bounds) * scale;
-  NSLog(@"%d %d", contextWidth, contextHeight);
+
   switch ([UIApplication sharedApplication].statusBarOrientation) {
     case UIInterfaceOrientationLandscapeLeft:
       contextRotate90 = false; contextFlipH = contextFlipV = true;
@@ -90,6 +104,7 @@
   SCNScene *scene = [SCNScene scene];
   SCNNode *cameraNode = [SCNNode node];
   cameraNode.camera = [SCNCamera camera];
+  cameraNode.name = @"defaultCamera";
   cameraNode.position = SCNVector3Make(0, 0, 10);
   [scene.rootNode addChildNode:cameraNode];
   _cameraNode = cameraNode;
@@ -102,7 +117,7 @@
   const char *resourcePath = [imagePath cStringUsingEncoding:NSUTF8StringEncoding];
   char *trackableConfig;
   // TODO: find out relation between height and h
-  asprintf(&trackableConfig, "2d;%s;%f", resourcePath, height);
+  asprintf(&trackableConfig, "2d;%s;%f", resourcePath, width);
   int trackableId = arController->addTrackable(trackableConfig);
   if (trackableId == -1) {
     ARLOGe("Error adding trackable.\n");
@@ -114,8 +129,11 @@
   SCNPlane *plane = [SCNPlane planeWithWidth:width height:height];
   plane.firstMaterial.diffuse.contents = [UIColor redColor];
   plane.firstMaterial.transparency = 0.5f;
+  plane.firstMaterial.doubleSided = YES;
+  plane.name = @"plane";
   SCNNode *trackableNode = [SCNNode nodeWithGeometry:plane];
-  trackableNode.position = SCNVector3Zero;
+  trackableNode.position = SCNVector3Make(0.0, 0.0, -10.0);
+  trackableNode.name = [NSString stringWithFormat:@"trackableNode_%d", trackableId];
   [self.cameraNode addChildNode:trackableNode];
   [self.trackableNodes addObject:trackableNode];
   free(trackableConfig);
@@ -145,7 +163,8 @@
 
   [self setupTrackables];
 
-  arController->get2DTracker().setMaxSimultaneousTrackedImages((int)trackableIds.size());
+  arController->get2dTracker()->setTwoDMultiMode(true);
+//  arController->get2DTracker().setMaxSimultaneousTrackedImages((int)trackableIds.size());
 
   // Start tracking.
   arController->startRunning(vconf, NULL, NULL, 0);
@@ -153,7 +172,7 @@
 
 - (void)tearDownAR {
   if (arController) {
-    arController->displayFrameFinal(0);
+    arController->drawVideoFinal(0);
     arController->shutdown();
     delete arController;
   }
@@ -173,44 +192,48 @@
 
 - (void)renderer:(id<SCNSceneRenderer>)renderer willRenderScene:(SCNScene *)scene atTime:(NSTimeInterval)time {
   [EAGLContext setCurrentContext:self.sceneView.eaglContext];
-
+  
   if (arController->isRunning()) {
     if (contextWasUpdated) {
-      arController->displayFrameInit(0);
-      arController->displayFrameSettings(0, contextWidth, contextHeight, contextRotate90, contextFlipH, contextFlipV, ARView::HorizontalAlignment::H_ALIGN_CENTRE, ARView::VerticalAlignment::V_ALIGN_CENTRE, ARView::ScalingMode::SCALE_MODE_FIT, viewport);
+      arController->drawVideoInit(0);
+      arController->drawVideoSettings(0, contextWidth, contextHeight, contextRotate90,
+                                      contextFlipH, contextFlipV,
+                                      ARVideoView::HorizontalAlignment::H_ALIGN_LEFT,
+                                      ARVideoView::VerticalAlignment::V_ALIGN_CENTRE,
+                                      ARVideoView::ScalingMode::SCALE_MODE_FIT,
+                                      viewport);
+
+      // Get the projection matrix
+      ARdouble projection[16];
+      arController->projectionMatrix(0, 100, -1000, projection);
+      projectionMatrix = SCNMatrix4Make(projection);
+      if (contextFlipV || contextFlipH) {
+        // Flip works fine for landscapeLeft versus landscapeRight
+        projectionMatrix = SCNMatrix4Scale(projectionMatrix, contextFlipH ? -1 : 1, contextFlipV ? -1 : 1, 1);
+      }
+      if (contextRotate90) {
+        // Rotation (SCNMatrix4Rotate takes radians)
+        projectionMatrix = SCNMatrix4Rotate(projectionMatrix, (M_PI * 90)/180, 0, 0, -1);
+      }
       contextWasUpdated = false;
     }
 
     // Display the current video frame to the current OpenGL context.
-    arController->displayFrame(0);
-    
-    // Get the projection matrix
-    SCNMatrix4 projectionMatrix;
-    SCNMatrix4 transformMatrix;
-    ARdouble projection[16];
-    arController->getProjectionMatrix(0, projection);
-    projectionMatrix = SCNMatrix4Make(projection);
-    
-    // Flip works fine for landscapeLeft versus landscapeRight
-    if (contextFlipV || contextFlipH) {
-      projectionMatrix = SCNMatrix4Scale(projectionMatrix, contextFlipH ? -1 : 1, contextFlipV ? -1 : 1, 1);
-    }
+    arController->drawVideo(0);
 
-    // Rotation (SCNMatrix4Rotate takes radians)
-    if (contextRotate90) {
-      projectionMatrix = SCNMatrix4Rotate(projectionMatrix, (M_PI * 90)/180, 0, 0, -1);
-    }
-    
+    SCNCamera *camera = self.sceneView.pointOfView.camera;
+
     // Look for trackables, and draw on each found one.
     size_t trackableCount = trackableIds.size();
+    float scaleFactor = 1000;
     for (size_t i = 0; i < trackableCount; i++) {
       // Find the trackable for the given trackable ID.
       ARTrackable *trackable = arController->findTrackable(trackableIds[i]);
-      SCNCamera *camera = self.cameraNode.camera;
       SCNNode *trackableNode = self.trackableNodes[i];
       if (trackable->visible) {
-        transformMatrix = SCNMatrix4Make(trackable->transformationMatrix);
+        SCNMatrix4 transformMatrix = SCNMatrix4Make(trackable->transformationMatrix);
         camera.projectionTransform = projectionMatrix;
+        transformMatrix = SCNMatrix4Scale(transformMatrix, scaleFactor, scaleFactor, scaleFactor);
         trackableNode.transform = transformMatrix;
         trackableNode.opacity = 1;
       } else {
